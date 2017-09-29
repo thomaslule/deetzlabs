b38ba184d326863c640a1d93127be4e03baeb5bc
thomaslule <thomas@lule.fr>
2017-09-29 13:56:51 +0200

    better error handling

--- showAchievement.js
+++ showAchievement.js
@@ -3,6 +3,17 @@ const clone = require('clone');
 const logger = require('./logger');
 const config = require('./config');
 
+const handleErrors = (error, response) => {
+  if (error) {
+    logger.error(error);
+    return error;
+  } else if (!(`${response.statusCode}`).startsWith('2')) {
+    logger.error('got a status code %s here', response.statusCode);
+    return new Error(`request failed, status code was ${response.statusCode}`);
+  }
+  return null;
+};
+
 module.exports = (achievement, callback) => {
   logger.info('send show achievement command', achievement);
   const requestBody = clone(achievement);
@@ -11,10 +22,8 @@ module.exports = (achievement, callback) => {
     uri: `${config.twitch_achievements.url}/achievement`,
     method: 'POST',
     json: requestBody,
-  }, (error) => {
-    if (error) {
-      logger.error(error);
-    }
-    callback(error);
+  }, (error, response) => {
+    const err = handleErrors(error, response);
+    callback(err);
   });
 };
