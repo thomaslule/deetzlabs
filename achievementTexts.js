c1092d4271a5692eb909b3c99e3b8a375eb9f4d2
thomaslule <thomas@lule.fr>
2017-10-01 13:14:55 +0200

    mécène

--- achievementTexts.js
+++ achievementTexts.js
@@ -4,4 +4,5 @@ module.exports = {
   'Suédois LV1': 'Hej %USER% !',
   Fossoyeuse: '%USER% est un peu sadique...',
   'Pom-pom girl': 'Merci pour tes encouragements %USER% !',
+  Mécène: 'Cool ! Merci pour ton soutien %USER%',
 };
