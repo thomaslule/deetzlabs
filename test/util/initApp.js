4f0a598ef90d7d634f955b9014962be43e2a5527
thomaslule <thomas@lule.fr>
2017-10-07 12:09:27 +0200

    no log while running tests

--- test/util/initApp.js
+++ test/util/initApp.js
@@ -1,8 +1,10 @@
 const storage = require('node-persist');
 const deetzlabs = require('../../deetzlabs');
 const httpServer = require('../../httpServer');
+const logger = require('../../logger');
 
 module.exports = () => {
+  logger.clear();
   storage.initSync({
     dir: './.node-persist/test_storage',
   });
