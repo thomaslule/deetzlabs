7b0744f390e7505afa0739a7f14209a2827c27bf
thomaslule <thomas@lule.fr>
2017-10-08 23:39:42 +0200

    trim 4 command detection

--- util/isCommand.js
+++ util/isCommand.js
@@ -1,1 +1,1 @@
-module.exports = (command, message) => message === command || message.startsWith(`${command} `);
+module.exports = (command, message) => message.trim() === command || message.trim().startsWith(`${command} `);
