87c5b04034ab776215866ae7c7cd69c65ffd2f92
thomaslule <thomas@lule.fr>
2018-04-18 12:07:12 +0200

    comments in config

--- config/default.js
+++ config/default.js
@@ -3,10 +3,10 @@ module.exports = {
   db_url: 'postgresql://postgres:admin@localhost:5432/deetzlabs',
   channel: 'some_twitch_channel',
   client_id: '', // get it by registering a twitch app https://www.twitch.tv/kraken/oauth2/clients/new
-  client_secret: '',
+  client_secret: '', // secret of your registered twitch app
   streamer_token: '', // create your token here https://twitchapps.com/tmi/
-  bot_name: '', // twitch account login
-  bot_token: '', // create your token here https://twitchapps.com/tmi/
+  bot_name: '', // twitch bot login
+  bot_token: '', // create your bot's token here https://twitchapps.com/tmi/
   secret: '', // any random string
   protect_api: true,
   logins: {
