c8f3ac8eea50b516e3bda324b993d74359553f6c
thomaslule <thomas@lule.fr>
2018-04-18 11:45:56 +0200

    remove config-public

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,3 +1,22 @@
 const express = require('express');
+const { Router } = require('express');
+const config = require('config');
 
-module.exports = () => express.static('src/widgets/public');
+module.exports = () => {
+  const router = Router();
+  router.get('/config.js', (req, res, next) => {
+    try {
+      const publicConfig = {
+        channel: config.get('channel'),
+        client_id: config.get('client_id'),
+      };
+      res.type('application/javascript');
+      res.send(`window.config = ${JSON.stringify(publicConfig)}`);
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.use(express.static('src/widgets/public'));
+
+  return router;
+};
--- src/widgets/public/config-public.template.js
+++
@@ -1,4 +0,0 @@
-window.config = {
-  channel: '', // just the channel name
-  client_id: '', // get it by registering a twitch app https://www.twitch.tv/kraken/oauth2/clients/new
-};
