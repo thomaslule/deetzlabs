805642612cb400ff4ab1a48c7ab7ef47de29367a
thomaslule <thomas@lule.fr>
2018-04-24 19:14:53 +0200

    fix 404 when accessing directly to a /admin/something url

--- src/admin.js
+++ src/admin.js
@@ -2,10 +2,13 @@ const { Router } = require('express');
 const express = require('express');
 const path = require('path');
 
+const adminPath = path.resolve(require.resolve('deetzlabs-web'), '..');
+
 module.exports = () => {
   const router = Router();
   router.get('/package.json', (req, res) => { res.sendStatus(404); });
-  router.use(express.static(path.resolve(require.resolve('deetzlabs-web'), '..')));
+  router.use(express.static(adminPath));
+  router.get('*', (req, res) => { res.sendfile(`${adminPath}/index.html`); });
 
   return router;
 };
