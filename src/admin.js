905948d652630125ee05638b128c038713f16c3b
thomaslule <thomas@lule.fr>
2018-04-24 11:58:11 +0200

    require resolve admin build

--- src/admin.js
+++ src/admin.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const express = require('express');
+const path = require('path');
 
 module.exports = () => {
   const router = Router();
   router.get('/package.json', (req, res) => { res.sendStatus(404); });
-  router.use(express.static(`${__dirname}/../node_modules/deetzlabs-web`));
+  router.use(express.static(path.resolve(require.resolve('deetzlabs-web'), '..')));
 
   return router;
 };
