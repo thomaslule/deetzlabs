c296ba65b8a8844ecd4acd65a1678867a4f70c61
thomaslule <thomas@lule.fr>
2018-11-04 20:54:49 +0100

    remove unused dependency

--- src/third-party-types.d.ts
+++ src/third-party-types.d.ts
@@ -1,7 +1,1 @@
 declare module 'express-http-proxy';
-
-declare module "stream-filter" {
-  import { Transform } from "stream";
-
-  export function obj(filter: (data: any) => boolean): Transform;
-}
