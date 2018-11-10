0cecda7988f0de32046ad935422d3b69ec51034b
thomaslule <thomas@lule.fr>
2018-11-10 17:59:35 +0100

    one sql reset database script

--- src/reset-database.ts
+++ src/reset-database.ts
@@ -2,42 +2,41 @@ import { Pool } from "pg";
 
 export async function resetDatabase(connectionString = "postgresql://postgres:admin@localhost:5432/deetzlabs_test") {
   const db = new Pool({ connectionString });
-  await db.query("drop table if exists events");
-  await db.query(`
-    create table events(
-      event_id serial primary key,
-      aggregate char(50) not null,
-      id text not null,
-      sequence integer not null,
-      event json not null,
-      constraint uc_events unique(aggregate, id, sequence)
-    );
-  `);
-  await db.query("drop table if exists projections");
-  await db.query(`
-    create table projections(
-      name text not null,
-      key text not null,
-      value json not null,
-      primary key(name, key)
-    );
-  `);
-  await db.query("drop table if exists viewers");
-  await db.query(`
-    create table viewers(
-      id text primary key,
-      name text not null default '',
-      last_action timestamptz not null
-    );
-  `);
-  await db.query("drop table if exists achievements");
-  await db.query(`
-    create table achievements(
-      viewerId text not null,
-      achievement text not null,
-      date timestamptz not null,
-      primary key(viewerId, achievement)
-    );
-  `);
+  await db.query(sql);
   await db.end();
 }
+
+const sql = `
+drop table if exists events;
+create table events(
+  event_id serial primary key,
+  aggregate char(50) not null,
+  id text not null,
+  sequence integer not null,
+  event json not null,
+  constraint uc_events unique(aggregate, id, sequence)
+);
+
+drop table if exists projections;
+create table projections(
+  name text not null,
+  key text not null,
+  value json not null,
+  primary key(name, key)
+);
+
+drop table if exists viewers;
+create table viewers(
+  id text primary key,
+  name text not null default '',
+  last_action timestamptz not null
+);
+
+drop table if exists achievements;
+create table achievements(
+  viewerId text not null,
+  achievement text not null,
+  date timestamptz not null,
+  primary key(viewerId, achievement)
+);
+`;
