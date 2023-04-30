b0bf76e23cb28bf220b420582c598c7afb003355
thomaslule <thomas@lule.fr>
2023-04-30 12:26:56 +0200

    test: create a docker-compose for develop

--- test/develop.ts
+++ test/develop.ts
@@ -6,19 +6,18 @@ import { testOptions } from "./test-options";
 
 async function start() {
   const db = new Pool({
-    connectionString:
-      "postgresql://postgres:admin@localhost:5432/deetzlabs_test"
+    connectionString: process.env.DEETZLABS_db_url,
   });
   const schema = readFileSync("./src/schema.sql", "utf8");
   await db.query(schema);
   await db.end();
   const deetzlabs = new Deetzlabs({
     ...testOptions,
-    ...secretOptions
+    ...secretOptions,
   });
   await deetzlabs.start();
 }
 
-start().catch(err => {
+start().catch((err) => {
   console.log(err);
 });
--- test/test-options.ts
+++ test/test-options.ts
@@ -5,59 +5,59 @@ const isGG = (event: Event) =>
   event.type === "sent-chat-message" && event.message.gg;
 
 export const testOptions: Partial<Options> = {
-  db_url: "postgresql://postgres:admin@localhost:5432/deetzlabs_test",
+  db_url: process.env.DEETZLABS_db_url,
   protect_api: false,
   log_level: "debug",
   log_to_file: false,
   log_to_console: true,
-  messageToObject: message => ({
+  messageToObject: (message) => ({
     commandsCommand: message === "!commands" ? true : undefined,
     achievementsCommand: message === "!achievements" ? true : undefined,
-    gg: message.startsWith("gg") ? true : undefined
+    gg: message.startsWith("gg") ? true : undefined,
   }),
   achievements: {
     cheerleader: {
       name: "Cheerleader",
       text: "Thank you %USER%!",
       description: "Automatically distributed when a viewer sends bits",
-      distributeWhen: (state, event) => event.type === "cheered"
+      distributeWhen: (state, event) => event.type === "cheered",
     },
     supporter: {
       name: "Supporter",
       text: "Thank you %USER%!",
       description: "Automatically distributed when a viewer says gg 5 times",
       reducer: (state = 0, event) => (isGG(event) ? state + 1 : state),
-      distributeWhen: (state, event) => isGG(event) && state >= 5
+      distributeWhen: (state, event) => isGG(event) && state >= 5,
     },
     host: {
       name: "Host",
       text: "Thank you %USER%!",
       description: "Automatically distributed when a viewer hosts or raid",
       distributeWhen: (state, event) =>
-        event.type === "hosted" || event.type === "raided"
-    }
+        event.type === "hosted" || event.type === "raided",
+    },
   },
   commands: [
     {
-      when: event =>
+      when: (event) =>
         event.type === "sent-chat-message" && event.message.commandsCommand,
-      say: () => "Say !achievements to see your current achievements"
+      say: () => "Say !achievements to see your current achievements",
     },
     {
-      when: event =>
+      when: (event) =>
         event.type === "sent-chat-message" && event.message.achievementsCommand,
       say: ({ viewerName, viewerAchievements, options }) => {
         const achievements = viewerAchievements
-          .map(a => options.achievements[a].name)
+          .map((a) => options.achievements[a].name)
           .join(", ");
         return viewerAchievements.length === 0
           ? `${viewerName} doesn't have any achievement but their time will come!`
           : `Congratulations ${viewerName} for your achievements: ${achievements}`;
-      }
+      },
     },
     {
-      when: event => event.type === "followed",
-      say: ({ viewerName }) => `Welcome ${viewerName}!`
-    }
-  ]
+      when: (event) => event.type === "followed",
+      say: ({ viewerName }) => `Welcome ${viewerName}!`,
+    },
+  ],
 };
